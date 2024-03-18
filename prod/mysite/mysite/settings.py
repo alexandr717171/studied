4c3ca793792a33b14426758f7d5bcf36cd08fe5f
alexandr <akvonsid@gmail.com>
2024-03-18 22:05:38 +0300

    add collectstatic in all-statics and change STATIC_ROOT

--- prod/mysite/mysite/settings.py
+++ prod/mysite/mysite/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 from os import getenv
@@ -166,7 +166,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'all_static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'uploads'
 # DEFAULT_FILE_STORAGE =
